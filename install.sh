2e003cb4970fc8d285f1a7205dac4d5a69b55850
Alex <alex@recordsonribs.com>
2015-02-09 19:56:58 +0000

    Update installation script.

--- install.sh
+++ install.sh
@@ -15,5 +15,21 @@ ln -s `pwd`/zsh ~/.vim
 # Set Mac OS defaults we like.
 ./setup/osx
 
+# # Install Homebrew.
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+# # Install Homebrew bundler.
+brew tap Homebrew/brewdler
+
+# Ensure we aren't installing everything somewhere a bit wacky.
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+# # Install everything else.
+brew brewdle
+
+brew cleanup
+
 echo 'Done'
 
