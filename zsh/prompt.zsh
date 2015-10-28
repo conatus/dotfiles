af01adc4343996848e6319059cef849bbdf1edec
Alex <alex@yplanapp.com>
2015-10-28 17:01:37 +0000

    Very simple ZSH VCS prompt using internals

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,40 +1,3 @@
-git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$(/usr/bin/git name-rev --name-only HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  /usr/bin/git cherry -v origin/$(git_branch) 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
 title(){
   printf "\033]0;%s\007" "$1"
 }
@@ -51,21 +14,15 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 }
 
-precmd(){
-  
-}
-
 preexec(){
   title `pwd`
 }
 
 set_prompt () {
-  # Could use preexec or precmd
-  #title `pwd`
-
-  export PROMPT=$'\n$(user_name) at $(host_name) in $(directory_name) $(git_dirty)$(need_push)\n› '
+  export PROMPT=$'\n$(user_name) at $(host_name) in $(directory_name) ${vcs_info_msg_0_}\n› '
 }
 
 precmd() {
+  vcs_info
   set_prompt
 }
--- zsh/set_options.zsh
+++ zsh/set_options.zsh
@@ -7,3 +7,8 @@ setopt always_to_end            # When complete from middle, move cursor
 setopt prompt_subst
 setopt auto_cd
 setopt extendedglob
+
+setopt prompt_subst
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:git*' formats "on %{$fg_bold[green]%}%b%{$reset_color%}"
